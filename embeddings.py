058783a29e298ccea0e9ed68609c1a539643a719
Lucas Marques <lucasmsguimaraes@gmail.com>
2023-05-22 23:50:53 -0400

    Add word/doc  logid and doc/set logic

--- embeddings.py
+++ embeddings.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
-import gensim
 from pathlib import Path
 from gensim.models import Word2Vec
 from sklearn.cluster import KMeans
+import numpy as np
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer
+import torch
+from transformers import BertTokenizer, BertModel
 
 downloads_path = str(Path.home() / "Downloads")
 
@@ -36,3 +40,67 @@ for doc_tokens in data['tokens']:
 # Print the topics for each document
 for i, topic in enumerate(topics):
     print('Document {}: Topic {}'.format(i, topic))
+
+
+def document_level_topic_modeling(documents, num_topics):
+    # Load the pre-trained BERT model and tokenizer
+    model_name = 'bert-base-uncased'
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+    model = BertModel.from_pretrained(model_name)
+
+    # Tokenize the documents and obtain the BERT embeddings
+    input_ids = []
+    attention_masks = []
+    for document in documents:
+        encoded_dict = tokenizer.encode_plus(
+            document,
+            add_special_tokens=True,
+            max_length=512,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt'
+        )
+        input_ids.append(encoded_dict['input_ids'])
+        attention_masks.append(encoded_dict['attention_mask'])
+
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+
+    with torch.no_grad():
+        outputs = model(input_ids, attention_mask=attention_masks)
+
+    embeddings = outputs[0][:, 0, :].numpy()  # Use the [CLS] token's embedding as document representation
+
+    # Use Latent Dirichlet Allocation (LDA) for document-level topic modeling
+    lda = LatentDirichletAllocation(n_components=num_topics)
+    lda.fit(embeddings)
+
+    return lda
+
+# Example corpus for word-level topic modeling
+corpus = [
+    "This is the first document.",
+    "This document is the second document.",
+    "And this is the third one.",
+    "Is this the first document?"
+]
+
+# Example documents for document-level topic modeling
+documents = [
+    "This is the first document.",
+    "This document is the second document.",
+    "And this is the third one.",
+    "Is this the first document?"
+]
+
+# Set the number of topics and top words for word-level topic modeling
+num_topics_word_level = 2
+num_top_words_word_level = 3
+
+# Set the number of topics for document-level topic modeling
+num_topics_document_level = 2
+
+# Obtain word-level topics
+word_level_topics = word_level_topic_modeling(corpus, num_topics_word_level, num_top_words_word_level)
+print("Word-level Topics:")
+for topic_idx, topic
